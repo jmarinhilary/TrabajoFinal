68d4782bfc6152e164f7c892ef0e24a806576550
jmarinhilary <jmarinhilary@gmail.com>
2015-04-02 12:59:31 -0500

    Actualizacion

---
+++ Solucion 'OnlineShop'/OnlineShop.Web.Telerik/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OnlineShop.Web.Telerik.Startup))]
+namespace OnlineShop.Web.Telerik
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
