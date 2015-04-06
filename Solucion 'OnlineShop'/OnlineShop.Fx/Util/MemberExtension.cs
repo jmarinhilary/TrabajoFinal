3bf5aa0f24a6f83fe112578183cbfcbc6bf576c0
jmarinhilary <jmarinhilary@gmail.com>
2015-04-05 21:21:19 -0500

    actualizacion

---
+++ Solucion 'OnlineShop'/OnlineShop.Fx/Util/MemberExtension.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Fx.Util
+{
+    public static class MemberExtension
+    {
+        public static string GetMemberName(this LambdaExpression expr)
+        {
+            var lexpr = expr;
+            MemberExpression mexpr = null;
+            if (lexpr.Body is MemberExpression)
+            {
+                mexpr = (MemberExpression)lexpr.Body;
+            }
+            else if (lexpr.Body is UnaryExpression)
+            {
+                mexpr = (MemberExpression)((UnaryExpression)lexpr.Body).Operand;
+            }
+            if (mexpr == null)
+            {
+                return null;
+            }
+            return mexpr.Member.Name;
+        }
+
+        public static string GetDisplayName<TModel>(string propertyName)
+        {
+            Type type = typeof(TModel);
+            // First look into attributes on a type and it's parents
+            DisplayAttribute attr;
+            attr = (DisplayAttribute)type.GetProperty(propertyName).GetCustomAttributes(typeof(DisplayAttribute), true).SingleOrDefault();
+            // Look for [MetadataType] attribute in type hierarchy
+            if (attr == null)
+            {
+                MetadataTypeAttribute metadataType = (MetadataTypeAttribute)type.GetCustomAttributes(typeof(MetadataTypeAttribute), true).FirstOrDefault();
+                if (metadataType != null)
+                {
+                    var property = metadataType.MetadataClassType.GetProperty(propertyName);
+                    if (property != null)
+                    {
+                        attr = (DisplayAttribute)property.GetCustomAttributes(typeof(DisplayNameAttribute), true).SingleOrDefault();
+                    }
+                }
+            }
+            return (attr != null) ? attr.Name : propertyName;
+        }
+
+        public static string GetDisplayName<TModel>(Expression<Func<TModel, object>> expression)
+        {
+            Type type = typeof(TModel);
+
+            string propertyName = expression.GetMemberName();
+            // First look into attributes on a type and it's parents
+            DisplayAttribute attr;
+            attr = (DisplayAttribute)type.GetProperty(propertyName).GetCustomAttributes(typeof(DisplayAttribute), true).SingleOrDefault();
+            // Look for [MetadataType] attribute in type hierarchy
+            if (attr == null)
+            {
+                MetadataTypeAttribute metadataType = (MetadataTypeAttribute)type.GetCustomAttributes(typeof(MetadataTypeAttribute), true).FirstOrDefault();
+                if (metadataType != null)
+                {
+                    var property = metadataType.MetadataClassType.GetProperty(propertyName);
+                    if (property != null)
+                    {
+                        attr = (DisplayAttribute)property.GetCustomAttributes(typeof(DisplayNameAttribute), true).SingleOrDefault();
+                    }
+                }
+            }
+            return (attr != null) ? attr.Name : propertyName;
+        }
+
+        public static bool GetReguiredAttr<TModel>(string propertyName)
+        {
+            Type type = typeof(TModel);
+            //MemberExpression memberExpression = (MemberExpression)expression.Body;
+            //string propertyName = ((memberExpression.Member is PropertyInfo) ? memberExpression.Member.Name : null);
+            // First look into attributes on a type and it's parents
+            RequiredAttribute attr;
+            attr = (RequiredAttribute)type.GetProperty(propertyName).GetCustomAttributes(typeof(RequiredAttribute), true).SingleOrDefault();
+            return (attr != null) ? true : false;
+        }
+    }
+}
---
+++ Solucion 'OnlineShop'/OnlineShop.Repositories/RepositoryBase.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShop.Domain;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using OnlineShop.Fx.Util;
+
+
+namespace OnlineShop.Repositories
+{
+    public abstract class RepositoryBase<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : EntityBase
+        where TContext : DbContext
+    {
+        protected TContext Context;
+
+        public RepositoryBase(TContext context)
+        {
+            this.Context = context;
+        }
+        public void Create(TEntity entity)
+        {
+            Context.Set<TEntity>().Add(entity);
+            Context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            Context.Entry(entity).State = EntityState.Modified;
+            Context.SaveChanges();
+        }
+
+        public IQueryable<TEntity> GetQueryable()
+        {
+            return Context.Set<TEntity>().AsQueryable();
+        }
+
+        public void Update(TEntity entity, Expression<Func<TEntity, object>>[] properties,
+            Expression<Func<TEntity, bool>> predicateExpression)
+        {
+            TEntity modelEntity = this.Context.Set<TEntity>().AsQueryable().Where(predicateExpression).FirstOrDefault();
+            Type modelType = typeof (TEntity);
+            foreach (var property in properties)
+            {
+                string propertyName = property.GetMemberName();
+                var propertyValue = modelType.GetProperty(propertyName).GetValue(entity);
+                modelType.GetProperty(propertyName).SetValue(modelEntity, propertyValue);
+            }
+            this.Context.SaveChanges();
+        }
+
+        public IQueryable<TEntity> GetQueryable(Expression<Func<TEntity, bool>> predicateExpression)
+        {
+            return Context.Set<TEntity>().AsQueryable().Where(predicateExpression);
+        }
+
+        public void Delete(System.Linq.Expressions.Expression<Func<TEntity, bool>> predicateExpression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            Context = null;
+        }
+        
+    }
+}
