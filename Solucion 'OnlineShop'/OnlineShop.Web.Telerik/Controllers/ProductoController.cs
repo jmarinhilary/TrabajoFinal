182b88013397a4a42b6d86c7dfbcf6beebf4e4d8
jmarinhilary <jmarinhilary@gmail.com>
2015-04-23 00:41:44 -0500

    as

---
+++ Solucion 'OnlineShop'/OnlineShop.Web.Telerik/Controllers/ProductoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Web.Telerik.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            //hola
+            return View();
+        }
+    }
+}
