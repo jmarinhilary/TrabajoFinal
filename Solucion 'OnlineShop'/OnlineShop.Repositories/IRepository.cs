73e863dd32c98d5d315420d7e1cfbcfde70913d8
jmarinhilary <jmarinhilary@gmail.com>
2015-04-25 09:48:55 -0500

    a

---
+++ Solucion 'OnlineShop'/OnlineShop.Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using OnlineShop.Domain;
+
+namespace OnlineShop.Repositories
+{
+    public interface IRepository<T> : IDisposable
+        where T : EntityBase
+    {
+        void Create(T entity);
+        IQueryable<T> Get();
+        void Update(T entity);
+        //void Update(T entity, Expression<Func<T, object>>[] properties, Expression<Func<T, bool>> predicateExpression);
+        //IQueryable<T> GetQueryable(Expression<Func<T, bool>> predicateExpression);
+        //void Delete(Expression<Func<T, bool>> predicateExpression);
+
+    }
+}
